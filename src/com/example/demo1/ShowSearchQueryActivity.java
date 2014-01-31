b353bb0a782266bb9a56a4b042fe1885111948cd
Melanie <melanie2292@gmail.com>
2014-01-30 22:30:45 -0600

    Demo1

---
+++ src/com/example/demo1/ShowSearchQueryActivity.java
@@ -0,0 +1,33 @@
+package com.example.demo1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class ShowSearchQueryActivity extends Activity {
+	public static final String QUERY = "query";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_search_query);
+		Intent intent= getIntent();
+		String queryText = intent.getStringExtra(QUERY);
+		if(queryText != null){
+			TextView txtMsg = (TextView)findViewById(R.id.txtMsg);
+			// mostrar el texto que reciba en el diccionario del intent como un extra y se llame query
+			txtMsg.setText(queryText);
+			
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.show_search_query, menu);
+		return true;
+	}
+
+}
